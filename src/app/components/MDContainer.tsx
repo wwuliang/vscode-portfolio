59b6492ea565b1c213122e2332ef341c4c0c48ef
wwuliang <williamwuliang22@gmail.com>
2023-12-31 17:57:39 -0500

    Fixed browser title name

--- src/app/components/MDContainer.tsx
+++ src/app/components/MDContainer.tsx
@@ -261,9 +261,9 @@ export default function MDContainer({ path }: Props) {
   useEffect(() => {
     let title = pathname.substring(1, pathname.length);
     title = title[0].toUpperCase() + title.substring(1);
-    document.title = `${process.env.REACT_APP_NAME!} | ${title}`;
+    document.title = `${process.env.REACT_APP_NAME || 'William Wuliang'} | ${title}`;
   }, [pathname]);
-
+  
   return (
     <Container>
       <ReactMarkdown
