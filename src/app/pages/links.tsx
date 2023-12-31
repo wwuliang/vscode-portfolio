6b60e18086727eaf3d216f309a892a286647d839
wwuliang <williamwuliang22@gmail.com>
2023-12-31 16:22:37 -0500

    Added files and added personal information

--- src/app/pages/links.tsx
+++ src/app/pages/links.tsx
@@ -1,40 +1,22 @@
-import {
-  FaMedium,
-  FaBlog,
-  FaGithub,
-  FaLinkedin,
-  FaEnvelope,
-} from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 export const links = [
   {
     index: 0,
     title: "Find me on Github",
-    href: "https://github.com/noworneverev",
+    href: "https://github.com/wwuliang",
     icon: <FaGithub />,
   },
   {
     index: 1,
     title: "Find me on LinkedIn",
-    href: "https://www.linkedin.com/in/yan-ying-liao/",
+    href: "https://www.linkedin.com/in/williamwuliang/",
     icon: <FaLinkedin />,
   },
   {
     index: 2,
     title: "Contact me via email",
-    href: "mailto:n9102125@gmail.com",
+    href: "mailto:williamwuliang22@gmail.com",
     icon: <FaEnvelope />,
-  },
-  {
-    index: 3,
-    title: "Find me on Medium",
-    href: "https://medium.com/@noworneverev",
-    icon: <FaMedium />,
-  },
-  {
-    index: 4,
-    title: "Find me on Blog",
-    href: "https://noworneverev.github.io/blog/",
-    icon: <FaBlog />,
-  },
+  }
 ];
