73a8d9f79d13eab2f26a9a03bfcc10f95a7f2d58
wwuliang <williamwuliang22@gmail.com>
2023-12-31 17:35:40 -0500

    Set default name

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -45,7 +45,7 @@ export default function Home({ setSelectedIndex }: Props) {
               display="flex"
               justifyContent={{ xs: "center", sm: "flex-start" }}
             >
-              <Typography variant="h3">{process.env.REACT_APP_NAME}</Typography>
+              <Typography variant="h3">{process.env.REACT_APP_NAME || 'William Wuliang'}</Typography>
             </Grid>
             <Grid
               display="flex"
